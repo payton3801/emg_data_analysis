b645dddde2d405ee02f1f135611a148bb5ebac98
payton3801 <payton3801@icloud.com>
2024-11-04 22:36:12 -0500

    Step 5: quartile scaling the data and making an overalid plot for 95 scale

--- real_data_preprocessing.py
+++ real_data_preprocessing.py
@@ -31,7 +31,7 @@ SAMPLE_RATE = 1/np.mean(intervals)
 NFFT = 50000 #Use sampling rate/NFFT = 0.1
 NPERSEG = 50000
 NOVERLAP = 10000 #Use .2 (NFFT) = NOVERLAP
-CHAN_IX = 0
+CHAN_IX = 5
 num_channels = rawdata.shape[1]
 
 #notch filter parameters
@@ -54,7 +54,7 @@ plt.gca().spines["right"].set_visible(False)
 plt.xlim([0,120])
 
 # %%
-#processing pipeline for one channel
+#processing pipeline for all channels
 #pre-processing step 1: notch filters at 60 Hz, 120 Hz, 240 Hz, 300 Hz, 420 Hz
 def apply_notch_filter(x, notch_frequencies, bandwidth, sample_rate):
     """ 
@@ -83,16 +83,18 @@ def apply_butterworth_filter(x):
     return butterworth
 
 # Rectify the EMG signal
-applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
-applied_butter = apply_butterworth_filter(applied_notch)
-rectifieddata = np.abs(applied_butter)
+for CHAN_IX in range(num_channels):
+    applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
+    applied_butter = apply_butterworth_filter(applied_notch)
+    rectifieddata = np.abs(applied_butter)
 
-notchdata = np.zeros_like(rawdata)
-for i in range(rawdata.shape[1]):
-    notchdata[:,i] = apply_notch_filter(rawdata[:,i], notch_frequencies, bandwidth, SAMPLE_RATE)
+    notchdata = np.zeros_like(rawdata)
+    for i in range(rawdata.shape[1]):
+        notchdata[:,i] = apply_notch_filter(rawdata[:,i], notch_frequencies, bandwidth, SAMPLE_RATE)
 
 # %%
 # Time vector for plotting
+
 duration = len(rectifieddata) / SAMPLE_RATE
 t = np.linspace(0, duration, len(rectifieddata), endpoint=False)
 
@@ -139,6 +141,9 @@ f, pxx = signal.welch(rectifieddata, nperseg=NPERSEG, nfft=NFFT, noverlap=NOVERL
 plt.loglog(f, pxx, '-o', alpha=0.4, color='g', markersize=2, label = "Rectified data")
 
 plt.legend()
+
+# %%
+#plot the low-pass filtered orig sampling rate data with the resampled data?
 
 # %%
 #rectified emg for all signals -- able to run independently after loading rawdata
@@ -205,63 +210,178 @@ plt.xlim([0,30])
 
 # %%
 #resampling EMG to 500 Hz
-SAMPLE_RATE = round(SAMPLE_RATE)
-TARGET_SAMPLE_RATE = 500
-DOWNSAMPLING = SAMPLE_RATE // TARGET_SAMPLE_RATE
-#resampled_data1 = signal.decimate(rectifieddata, q = DOWNSAMPLING, n=None, ftype='iir', axis=-1, zero_phase=True)
-    #check: axis = 1 and zero_phase = True?
-resampled_data = signal.resample_poly(rectifieddata, down=DOWNSAMPLING, up=1)
-duration = len(resampled_data) / TARGET_SAMPLE_RATE
-t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
+for CHAN_IX in range(num_channels):
+    # Apply notch filter
+    applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
+    
+    # Apply Butterworth filter
+    applied_butter = apply_butterworth_filter(applied_notch)
+    
+    # Rectify the EMG signal
+    rectifieddata = np.abs(applied_butter)
+    
+    # Resample the data
+    SAMPLE_RATE = round(SAMPLE_RATE)
+    TARGET_SAMPLE_RATE = 500
+    DOWNSAMPLING = SAMPLE_RATE // TARGET_SAMPLE_RATE
+    resampled_data = signal.resample_poly(rectifieddata, down=DOWNSAMPLING, up=1)
+    duration = len(resampled_data) / TARGET_SAMPLE_RATE
+    t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
 
-# Plotting the resampled data over time
-plt.figure(figsize=(10,4), dpi=200)
-plt.plot(t, rectifieddata, 'r--', color='k', alpha=0.7, marker = 'o', markersize=3, label='Original Data')
-#plt.plot(t2, resampled_data1, 'r--', color = 'b', alpha=0.7, marker='o', markersize=3, label='Decimate')
-plt.plot(t2, resampled_data, 'r--', color = 'g', alpha=0.7, marker='o', markersize=3, label='Resample-poly')
+    # Time vector for plotting
+    t = np.linspace(0, len(rectifieddata) / SAMPLE_RATE, len(rectifieddata), endpoint=False)
 
-plt.gca().spines["top"].set_visible(False)
-plt.gca().spines["right"].set_visible(False)
-plt.ylabel("Amplitude")
-plt.xlabel("Time (s)")
-plt.title(f"Resampled Rectified EMG for Muscle {channel_names[CHAN_IX]}")
+    # Plotting the resampled data over time
+    plt.figure(figsize=(10, 4), dpi=200)
+    plt.plot(t, rectifieddata, 'r--', color='k', alpha=0.7, marker='o', markersize=3, label='Original Data')
+    plt.plot(t2, resampled_data, 'r--', color='g', alpha=0.7, marker='o', markersize=3, label='Resample-poly')
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.title(f"Resampled Rectified EMG for Muscle {channel_names[CHAN_IX]}")
+    plt.tight_layout()
+    plt.xlim([0, 5])
+    plt.ylim([0, .33])  # for zooming in/ filling the whitespace
+
+    plt.legend()
+    plt.show()
+# %%
+#quartile clipping the data 
+for CHAN_IX in range(num_channels):
+    # Apply notch filter
+    applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
+    
+    # Apply Butterworth filter
+    applied_butter = apply_butterworth_filter(applied_notch)
+    
+    # Rectify the EMG signal
+    rectifieddata = np.abs(applied_butter)
+    
+    # Resample the data
+    SAMPLE_RATE = round(SAMPLE_RATE)
+    TARGET_SAMPLE_RATE = 500
+    DOWNSAMPLING = SAMPLE_RATE // TARGET_SAMPLE_RATE
+    resampled_data = signal.resample_poly(rectifieddata, down=DOWNSAMPLING, up=1)
+    duration = len(resampled_data) / TARGET_SAMPLE_RATE
+    t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
+
+    # Quartile clipping the data
+    quartiled_data_9999 = np.quantile(resampled_data, .9999)
+    quartiled_data_99 = np.quantile(resampled_data, .99)
+    quartiled_data_95 = np.quantile(resampled_data, .95)
+    quartiled_data_90 = np.quantile(resampled_data, .90)
+    clipped_data = np.clip(resampled_data, a_max=quartiled_data_9999, a_min=None)
+    clipped_data1 = np.clip(resampled_data, a_max=quartiled_data_99, a_min=None)
+    clipped_data2 = np.clip(resampled_data, a_max=quartiled_data_95, a_min=None)
+    clipped_data3 = np.clip(resampled_data, a_max=quartiled_data_90, a_min=None)
+    duration = len(clipped_data) / TARGET_SAMPLE_RATE
+    t3 = np.linspace(0, duration, len(clipped_data), endpoint=False)
+
+    # Plotting
+    plt.figure(figsize=(10, 4), dpi=200)
+    plt.plot(t2, resampled_data, 'r--', color='r', alpha=0.7, marker='o', markersize=3, label='Original Data')
+    plt.plot(t3, clipped_data, 'r--', color='g', alpha=0.7, marker='o', markersize=3, label='99.99')
+    plt.plot(t3, clipped_data1, 'r--', color='b', alpha=0.7, marker='o', markersize=3, label='99')
+    plt.plot(t3, clipped_data2, 'r--', color='m', alpha=0.7, marker='o', markersize=3, label='95')
+    plt.plot(t3, clipped_data3, 'r--', color='k', alpha=0.7, marker='o', markersize=3, label='90')
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.title(f"Quantile Clipping for Muscle {channel_names[CHAN_IX]}")
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+# %%
+#plotting quantile scaling the data, using .95 as the clip
+for CHAN_IX in range(num_channels):
+    # Apply notch filter
+    applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
+    
+    # Apply Butterworth filter
+    applied_butter = apply_butterworth_filter(applied_notch)
+    
+    # Rectify the EMG signal
+    rectifieddata = np.abs(applied_butter)
+    
+    # Resample the data
+    SAMPLE_RATE = round(SAMPLE_RATE)
+    TARGET_SAMPLE_RATE = 500
+    DOWNSAMPLING = SAMPLE_RATE // TARGET_SAMPLE_RATE
+    resampled_data = signal.resample_poly(rectifieddata, down=DOWNSAMPLING, up=1)
+    quartiled_data_9999 = np.quantile(resampled_data, .9999)
+    clipped_data = np.clip(resampled_data, a_max=quartiled_data_9999, a_min=None)
+
+
+    # Quartile clipping the data
+    quartiled_data_95 = np.quantile(clipped_data, .95)
+    duration = len(resampled_data) / TARGET_SAMPLE_RATE
+    t = np.linspace(0, duration, len(resampled_data), endpoint=False)
+    normalized_data = clipped_data / quartiled_data_95
+
+    # Plotting time series data
+    fig, axes = plt.subplots(2, 1, figsize=(20, 8), dpi=200)
+
+    # Plot of clipped data
+    axes[0].plot(t, clipped_data, alpha=0.5, label='Clipped Data')
+    axes[0].axhline(y=quartiled_data_95, color='k', linestyle='--', label='95th Quantile')
+    axes[0].set_xlim([0, duration])
+    axes[0].set_title(f"Time plots of Muscle {channel_names[CHAN_IX]} Clipped Data")
+    axes[0].legend()
+
+    # Plot of normalized data
+    axes[1].plot(t, normalized_data, alpha=0.5, label='Normalized Data')
+    axes[1].axhline(y=1, color='k', linestyle='--', label='95th Quantile')
+    axes[1].set_xlim([0, duration])
+    #axes[1].set_ylim([0, 5])  
+    axes[1].set_title(f"Time plots of Muscle {channel_names[CHAN_IX]} Normalized Data")
+    axes[1].legend()
+
+    # Show the plots
+    plt.tight_layout()
+    plt.show()
+
+
+# %%
+#step 5: plotting all muscle traces on top of each other
+fig, axes = plt.subplots(2, 1, figsize=(20, 8), dpi=200)
+
+for CHAN_IX in range(num_channels):
+    applied_notch = apply_notch_filter(rawdata[:, CHAN_IX], notch_frequencies, bandwidth, SAMPLE_RATE)
+    applied_butter = apply_butterworth_filter(applied_notch)
+    rectifieddata = np.abs(applied_butter)
+    
+    # Resample the data
+    SAMPLE_RATE = round(SAMPLE_RATE)
+    TARGET_SAMPLE_RATE = 500
+    DOWNSAMPLING = SAMPLE_RATE // TARGET_SAMPLE_RATE
+    resampled_data = signal.resample_poly(rectifieddata, down=DOWNSAMPLING, up=1)
+    quartiled_data_9999 = np.quantile(resampled_data, .9999)
+    clipped_data = np.clip(resampled_data, a_max=quartiled_data_9999, a_min=None)
+
+    # Quartile clipping the data
+    quartiled_data_95 = np.quantile(clipped_data, .95)
+    duration = len(resampled_data) / TARGET_SAMPLE_RATE
+    t = np.linspace(0, duration, len(resampled_data), endpoint=False)
+    normalized_data = clipped_data / quartiled_data_95
+
+    # Plot of clipped data
+    axes[0].plot(t, clipped_data, alpha=0.5, label=f'Clipped Data {channel_names[CHAN_IX]}')
+    axes[0].axhline(y=quartiled_data_95, color='k', linestyle='--', label='95th Quantile')
+    axes[0].set_xlim([0, duration])
+    axes[0].set_title("Time plots of Clipped Data")
+
+    # Plot of normalized data
+    axes[1].plot(t, normalized_data, alpha=0.5, label=f'Normalized Data {channel_names[CHAN_IX]}')
+    axes[1].axhline(y=1, color='k', linestyle='--', label='95th Quantile')
+    axes[1].set_xlim([0, duration])
+    axes[1].set_title("Time plots of Normalized Data")
+
+# Show the plots
 plt.tight_layout()
-plt.xlim([0, 5])
-plt.ylim([0, .33]) #for zooming in/ filling the whitespace
-
-#plt.xlim([0.3, 0.4])
-#plt.ylim([0, .33]) #for looking super close
-
-plt.legend()
 plt.show()
 
 # %%
-#quartile clipping the data by the 99.99th percentile
-quantiled_data = np.quantile(resampled_data, .9999)
-quantiled_data1 = np.quantile(resampled_data, .99)
-quantiled_data2 = np.quantile(resampled_data, .95)
-quantiled_data3 = np.quantile(resampled_data, .90)
-clipped_data = np.clip(resampled_data, a_max = quantiled_data, a_min=None)
-clipped_data1 = np.clip(resampled_data, a_max = quantiled_data1, a_min=None)
-clipped_data2 = np.clip(resampled_data, a_max = quantiled_data2, a_min=None)
-clipped_data3 = np.clip(resampled_data, a_max = quantiled_data3, a_min=None)
-duration = len(clipped_data) / TARGET_SAMPLE_RATE
-t3 = np.linspace(0, duration, len(clipped_data), endpoint=False)
-
-
-#plotting
-plt.figure(figsize=(10,4), dpi=200)
-plt.plot(t2, resampled_data, 'r--', color='r', alpha=0.7, marker = 'o', markersize=3, label='Original Data')
-plt.plot(t3, clipped_data, 'r--', color='g', alpha=0.7, marker='o', markersize=3, label='99.99')
-plt.plot(t3, clipped_data1, 'r--', color = 'b', alpha=0.7, marker='o', markersize=3, label='99')
-plt.plot(t3, clipped_data2, 'r--', color = 'm', alpha=0.7, marker='o', markersize=3, label='95')
-plt.plot(t3, clipped_data3, 'r--', color = 'k', alpha=0.7, marker='o', markersize=3, label='90')
-plt.gca().spines["top"].set_visible(False)
-plt.gca().spines["right"].set_visible(False)
-plt.ylabel("Amplitude")
-plt.xlabel("Time (s)")
-plt.title(f"Quantile Clipping for Muscle {channel_names[CHAN_IX]}")
-plt.tight_layout()
-plt.legend()
-
- # %%
