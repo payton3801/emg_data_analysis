6f02ee925e78bb50e194dd266e26f4352c4ec2fe
payton3801 <payton3801@icloud.com>
2024-11-23 21:46:40 -0500

    Butterworth low pass filtered and resampled the data

--- kinematics_data_preprocessing.py
+++ kinematics_data_preprocessing.py
@@ -15,52 +15,42 @@ for key in kindata:
         print(f"Shape: {kindata[key].shape}")
 
 joints = kindata['joints_raw_nonSeg']
-
+num_joints = joints.shape[0]
+cmap = plt.get_cmap('viridis')
+colors = cmap(np.linspace(0, 1, num_joints))
 
 # %%
 #calculate sampling rate
 time_vector = kindata['t_kin'].flatten()
 intervals = np.diff(time_vector)
 SAMPLE_RATE = 1 / np.mean(intervals)
+SAMPLE_RATE = round(SAMPLE_RATE)
+TARGET_SAMPLE_RATE = 500
+
 joint_names_1 = kindata['joints_names'].flatten()
 joint_names = [name[0] for name in joint_names_1]
 JOINT_IX = 1
+BLIP_DURATION = 0.03
+BLIP_SAMPLES = int(BLIP_DURATION * TARGET_SAMPLE_RATE) 
 
 # %%
-#extracting the 2d arrays
-for i, element in enumerate(joints):
-    inner_array = element[0]
-    print(inner_array.shape)
+#low pass filtering the data 
+def apply_butterworth_filter(x, sample_rate=SAMPLE_RATE, cutoff=40.0, order=4, padlen = 150):
+    """
+        apply 4th order butterworth low pass filter with cutoff at 40 Hz
+        x = signal data being filtered
+        sample_rate = samples per second of the data
+        cutoff = cutoff frequency
+        order = filter order
+    """
+    #padded_x = np.pad(x, padlen, mode = 'edge')
 
-# %%
-plt.figure(figsize=(10, 6))
+    b, a = signal.butter(4, 40.0, btype='low', analog=False, fs=SAMPLE_RATE)
+    butterworth = signal.filtfilt(b, a, x)
 
-for i, joint in enumerate(joints):
-    # Flatten the inner array to 1D if necessary
-    joint_flat = np.concatenate(joint) #ravel #combines into a single array, flattened the array
-    min_length = min(len(time_vector), len(joint_flat)) #allows time array and combined joint array to have the same length for plotting
-    plt.plot(time_vector[:min_length], joint_flat[:min_length], marker= 'o',alpha=0.4, label=joint_names[i])
+    #butterworth = butterworth[padlen:-padlen]
 
-plt.xlabel('Time (s)') 
-plt.ylabel('Kinematic Angle (degrees)')  
-plt.title('Kinematic Angle per Joint Over Time')
-plt.xlim([0, 1])
-
-plt.legend()
-plt.show()
-
-
-# %%
-#resampling the data
-num_joints = joints.shape[0]
-plt.figure(figsize=(7, 4), dpi=200)
-cmap = plt.get_cmap('viridis')
-colors = cmap(np.linspace(0, 1, num_joints))
-
-SAMPLE_RATE = round(SAMPLE_RATE)
-TARGET_SAMPLE_RATE = 500
-UPSAMPLING = 5
-DOWNSAMPLING = 2
+    return butterworth
 
 
 for JOINT_IX in range(num_joints):
@@ -68,16 +58,61 @@ for JOINT_IX in range(num_joints):
     if joint_data.shape[1] == 2:
         for col in range(2):
             
-            resampled_data = signal.resample_poly(joint_data[:, col], up=UPSAMPLING, down= DOWNSAMPLING)
+            applied_butter = apply_butterworth_filter(joint_data[:, col], SAMPLE_RATE)
+
+            plt.plot(time_vector, applied_butter, alpha=0.7, marker= 'o', color = colors[JOINT_IX], markersize=3, label=joint_names[JOINT_IX])
+
+    else:
+
+        applied_butter = apply_butterworth_filter(joint_data[:, 0])
+
+        plt.plot(time_vector, applied_butter, alpha=0.7, marker= 'o', color = colors[JOINT_IX], markersize=3, label=joint_names[JOINT_IX])
+
+plt.xlabel('Time (s)') 
+plt.ylabel('Kinematic Angle (degrees)')  
+plt.title('Kinematic Angle per Joint Over Time')
+plt.xlim([0, 5])
+
+plt.legend()
+plt.show()
+
+# %%
+#extracting the 2d arrays
+for i, element in enumerate(joints):
+    inner_array = element[0]
+    print(inner_array.shape)
+
+
+# %%
+#resampling and filtering the data
+plt.figure(figsize=(7, 4), dpi=200)
+
+
+UPSAMPLING = 5
+DOWNSAMPLING = 2
+
+for JOINT_IX in range(num_joints):
+    joint_data = joints[JOINT_IX][0]
+    if joint_data.shape[1] == 2:
+        for col in range(2):
+
+
+            filtered_data = apply_butterworth_filter(joint_data[:, col])
+            resampled_data = signal.resample_poly(filtered_data, up=UPSAMPLING, down= DOWNSAMPLING) 
+
+
             duration = len(resampled_data) / TARGET_SAMPLE_RATE
             t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
 
             # Plotting the resampled data over time
-            plt.plot(t2, resampled_data, alpha=0.7, marker= 'o', color = colors[JOINT_IX], markersize=3, label=joint_names[JOINT_IX])
+            plt.plot(t2, resampled_data, alpha=0.7, marker= 'o', color = colors[JOINT_IX], markersize=3, label=f' {joint_names[JOINT_IX] } Col {col+1}')
 
     else:
 
-        resampled_data = signal.resample_poly(joint_data[:,0], up=UPSAMPLING, down= DOWNSAMPLING)
+        filtered_data = apply_butterworth_filter(joint_data[:, 0])
+        resampled_data = signal.resample_poly(filtered_data, up = UPSAMPLING, down = DOWNSAMPLING)
+
+
         duration = len(resampled_data) / TARGET_SAMPLE_RATE
         t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
 
@@ -88,14 +123,21 @@ plt.gca().spines["top"].set_visible(False)
 plt.gca().spines["right"].set_visible(False)
 plt.ylabel("Amplitude")
 plt.xlabel("Time (s)")
-plt.xlim([0, 1])
-plt.title(f"Resampled EMG")
+plt.xlim([0, 5])
+plt.title(f"Resampled and EMG")
 plt.tight_layout()
 plt.legend()
 plt.show()
 
-# %%
+
+
+
+
+
+
+# %% -
 #plotting both overlaid
+
 plt.figure(figsize=(10, 6))
 joint_names_1 = kindata['joints_names'].flatten()
 joint_names = [name[0] for name in joint_names_1]
@@ -103,38 +145,36 @@ joint_names = [name[0] for name in joint_names_1]
 for JOINT_IX in range(num_joints):
     # Access the inner 2D array
     joint_data = joints[JOINT_IX][0]
+    min_length_original = min(len(time_vector), len(joint_data))
+    plt.plot(time_vector[:min_length_original], joint_data[:min_length_original], linestyle='--', marker='o', color=colors[JOINT_IX], alpha=0.9, label=f'Original {joint_names[JOINT_IX]}')
+
     if joint_data.shape[1] == 2:
-
-        min_length_original = min(len(time_vector), len(joint_data))
-        plt.plot(time_vector[:min_length_original], joint_data[:min_length_original], linestyle='--', marker = 'o', color=colors[JOINT_IX], alpha=0.9, label=f'Original {joint_names[JOINT_IX]}')
-
-
         for col in range(2):
-
             # Resample the data
-            resampled_data = signal.resample_poly(joint_data[:, col], up=UPSAMPLING, down=DOWNSAMPLING)
+            filtered_data = apply_butterworth_filter(joint_data[:, col])
+            resampled_data = signal.resample_poly(filtered_data, up=UPSAMPLING, down=DOWNSAMPLING)
+
             duration = len(resampled_data) / TARGET_SAMPLE_RATE
             t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
 
             # Ensure the lengths match for plotting
             min_length_resampled = min(len(t2), len(resampled_data))
 
-            # Plotting the original and resampled data over time
-            plt.plot(t2, resampled_data, linestyle='-', color = 'k', alpha=0.2, marker='o', label=f'Resampled {joint_names[JOINT_IX]} Col {col+1}')
+            # Plotting the resampled data over time
+            plt.plot(t2[:min_length_resampled], resampled_data[:min_length_resampled], linestyle='-', color='k', alpha=0.2, marker='o', label=f'Resampled {joint_names[JOINT_IX]} Col {col+1}')
     else:
+        # Resample the data
+        filtered_data = apply_butterworth_filter(joint_data[:, 0])
+        resampled_data = signal.resample_poly(filtered_data, up=UPSAMPLING, down=DOWNSAMPLING)
 
-        resampled_data = signal.resample_poly(joint_data[:, 0], up=UPSAMPLING, down=DOWNSAMPLING)
         duration = len(resampled_data) / TARGET_SAMPLE_RATE
         t2 = np.linspace(0, duration, len(resampled_data), endpoint=False)
 
-            # Ensure the lengths match for plotting
-        min_length_original = min(len(time_vector), len(joint_data))
+        # Ensure the lengths match for plotting
         min_length_resampled = min(len(t2), len(resampled_data))
 
-            # Plotting the original and resampled data over time
-        plt.plot(time_vector[:min_length_original], joint_data[:min_length_original], linestyle='--', marker = 'o', color=colors[JOINT_IX], alpha=0.9, label=f'Original {joint_names[JOINT_IX]}')
-        plt.plot(t2, resampled_data, linestyle='-', color = 'k', alpha=0.2, marker='o', label=f'Resampled {joint_names[JOINT_IX]}')
-
+        # Plotting the resampled data over time
+        plt.plot(t2[:min_length_resampled], resampled_data[:min_length_resampled], linestyle='-', color='k', alpha=0.2, marker='o', label=f'Resampled {joint_names[JOINT_IX]}')
 
 # Set plot labels and title
 plt.xlabel('Time (s)')
@@ -143,7 +183,7 @@ plt.title('Original and Resampled Kinematic Angles for All Joints')
 plt.legend()
 plt.gca().spines["top"].set_visible(False)
 plt.gca().spines["right"].set_visible(False)
-plt.xlim([0,.5])
+plt.xlim([0, .5])
 plt.show()
 
 # %%
